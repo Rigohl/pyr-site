f6bfd800ff15a81df77d2468658c6b92b2c32b75
Rigohl <rigobertohula@gmail.com>
2025-06-14 00:59:41 -0600

    1

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,20 +1,14 @@
-import { https } from "firebase-functions";
-import next from "next";
-import path from "path";
-import { fileURLToPath } from "url";
+const functions = require("firebase-functions");
+const next = require("next");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const isDev = process.env.NODE_ENV !== "production";
-
-const server = next({
-  dev: isDev,
-  conf: { distDir: path.join(__dirname, "..", ".next") },
+const dev = process.env.NODE_ENV !== "production";
+const app = next({
+  dev,
+  conf: { distDir: ".next" },
 });
+const handle = app.getRequestHandler();
 
-const nextjsHandle = server.getRequestHandler();
-
-export const nextServer = https.onRequest((req, res) => {
-  return server.prepare().then(() => nextjsHandle(req, res));
+exports.nextServer = functions.https.onRequest(async (req, res) => {
+  await app.prepare();
+  return handle(req, res);
 });
