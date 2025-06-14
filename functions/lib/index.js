64eb38f72b8f3b53e895d3cebede6897b4b151ea
Rigohl <rigobertohula@gmail.com>
2025-06-13 23:41:20 -0600

    1,
    vamos

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,15 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nextServer = void 0;
-const https_1 = require("firebase-functions/v2/https");
-const next_1 = __importDefault(require("next"));
+import { onRequest } from "firebase-functions/v2/https";
+import next from "next";
 const dev = process.env.NODE_ENV !== "production";
-const app = (0, next_1.default)({ dev });
+const app = next({ dev });
 const handle = app.getRequestHandler();
-exports.nextServer = (0, https_1.onRequest)({ timeoutSeconds: 60 }, async (req, res) => {
+export const nextServer = onRequest({ timeoutSeconds: 60 }, async (req, res) => {
     await app.prepare();
     handle(req, res);
 });
