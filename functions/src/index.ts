3a92d4cc6acc9f4a42e7837d63acbaa3691570d3
Rigohl <rigobertohula@gmail.com>
2025-06-14 00:19:32 -0600

    1changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,29 @@
 import { onRequest } from "firebase-functions/v2/https";
-import { createServer } from "node:http";
 import next from "next";
+import path from "path";
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const isDev = process.env.NODE_ENV !== "production";
 
-export const nextServer = onRequest({ timeoutSeconds: 60 }, async (req, res) => {
-  await app.prepare();
-  handle(req, res);
+const nextServer = next({
+  dev: isDev,
+  // La ubicación del directorio de build de Next.js
+  conf: { distDir: path.join(process.cwd(), ".next") },
 });
+
+const nextjsHandle = nextServer.getRequestHandler();
+
+export const nextServer = onRequest(
+  {
+    timeoutSeconds: 60, // Aumenta el timeout para el primer arranque en frío
+    region: "us-central1",
+  },
+  async (req, res) => {
+    try {
+      await nextServer.prepare();
+      return await nextjsHandle(req, res);
+    } catch (error) {
+      console.error("Error handling request with Next.js:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
