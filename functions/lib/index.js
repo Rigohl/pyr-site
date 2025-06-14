1dc47e2a441f6c8cf58ebc494e7a5de69b22c8d9
Rigohl <rigobertohula@gmail.com>
2025-06-14 01:13:30 -0600

    1

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,18 @@
 import { onRequest } from "firebase-functions/v2/https";
 import next from "next";
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+import path from "path";
+import { fileURLToPath } from "url";
+// Soporte __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = next({
+    dev: false,
+    conf: {
+        distDir: path.join(__dirname, "..", ".next"),
+    },
+});
+const handler = app.getRequestHandler();
 export const nextServer = onRequest({ timeoutSeconds: 60 }, async (req, res) => {
     await app.prepare();
-    handle(req, res);
+    handler(req, res);
 });
