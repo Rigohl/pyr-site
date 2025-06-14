09d488b4669903fd995ca7ddb1cc466f4654781d
Rigohl <rigobertohula@gmail.com>
2025-06-13 18:38:05 -0600

    1
    TODO PARA FIREBASE

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,17 @@
-const functions = require("firebase-functions");
-const next = require("next");
+const { https } = require("firebase-functions");
+const { default: next } = require("next");
+const path = require("path");
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({
-  dev,
-  conf: { distDir: ".next" },
+const isDev = process.env.NODE_ENV !== "production";
+
+const server = next({
+  dev: isDev,
+  // La carpeta .next está un nivel arriba de la carpeta /functions
+  conf: { distDir: path.join("..", ".next") },
 });
-const handle = app.getRequestHandler();
 
-exports.nextServer = functions.https.onRequest(async (req, res) => {
-  await app.prepare();
-  return handle(req, res);
+const nextjsHandle = server.getRequestHandler();
+
+exports.nextServer = https.onRequest((req, res) => {
+  return server.prepare().then(() => nextjsHandle(req, res));
 });
